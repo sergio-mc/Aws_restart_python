b32c09a9b92d01793653a6435e6e0f0f35b513da
sergio-mc <sergiodev0106@gmail.com>
2021-04-23 21:35:11 +0200

    Ej 28/29

--- homework/ej28.py
+++ homework/ej28.py
@@ -1,25 +1,21 @@
-
-
-listin = {
-    "sergio":"640103006",
-    "cristina":"123134908",
-    "patricia":"765434321",
-    "javier":"002244330",
-}
+listin = {}
 
 userInput = ""
 
 while userInput != "5":
 
-    f = open("listin", "r")
-    line = f.readline()
-    lineSplited = line.split(",")
-    for i in lineSplited:
-        print(i)
+    try:
+        with open("listin", "r") as f:
+            for line in f:
+                linerStrip = line.rstrip(",\n")
+                lineSplited = linerStrip.split(",")
+                listin[lineSplited[0]] = lineSplited[1]
+    except:
+        print("No se pudo leer listin porque no existe")
 
     userInput = input("""
     ---------------------------------
-    # Crear fichero con listin: 1
+    # Crear fichero listin: 1
     # Consultar tlf cliente: 2
     # Añadir tlf nuevo cliente: 3
     # Eliminar tlf de un cliente: 4
@@ -43,16 +39,35 @@ while userInput != "5":
         
         
 
-    # elif userInput == "2":
-        
+    elif userInput == "2":
+        userPhoneConsult = input("Dime el nombre del telefono a consultar: ")
+        for cliente in listin:
+            if userPhoneConsult == cliente:
+                print(listin[cliente])
 
-        
+    elif userInput == "3":
+        addUserName = input("Dime el nombre del nuevo cliente: ")
+        addUserPhone = input("Dime el telefono del nuevo cliente: ")
+        f = open("listin", "a")
+        f.write(addUserName + "," +  addUserPhone + "\n")
+        f.close()
 
+    elif userInput == "4":
+        userRemove = input("Dime el nombre del usuario a eliminar: ")
         
+        try:
+            # leer archivo y guardarmelo para luego al usar open("w") y me vacie el listin poderlo rellenar con lo que tenia antes
+            with open("listin", "r") as x:
+                linesReadListin = x.readlines()
+            with open("listin", "w") as f:
+                for line in linesReadListin:
+                    linerStrip = line.partition(",")
+                    print(linerStrip[0])
+                    if linerStrip[0] != userRemove:
+                        f.write(line)
+            f.close()  
+        except:
+            print("error")
+                # if lineSplited[0] == userRemove:
 
-    # elif userInput == "3":
-        
-        
-        
-    # elif userInput == "4":
-        
+                
